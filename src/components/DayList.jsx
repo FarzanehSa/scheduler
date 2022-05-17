650ee7f10676817732cdbed2c511bbb70e7a63a7
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-05-17 06:42:25 -0700

    implement dayList component in storybook

---
+++ src/components/DayList.jsx
@@ -0,0 +1,22 @@
+import React, { useLayoutEffect } from "react";
+import DayListItem from "./DayListItem";
+
+export default function DayList(props) {
+
+  const dayListItems = props.days.map (day => {
+    return (
+      <DayListItem 
+        key={day.id}
+        name={day.name}
+        spots={day.spots}
+        setDay={props.setDay}
+        selected={props.day === day.name}
+      />);
+  });
+
+  return (
+    <ul>
+      {dayListItems}
+    </ul>
+  );
+}
