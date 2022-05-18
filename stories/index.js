85bd079e8609b6d668392035a01faf33676bc019
FarzanehSadegh <farzanehsadegh@gmail.com>
2022-05-18 13:57:05 -0700

    add Appointment Compotent, and Empty, Header views

--- stories/index.js
+++ stories/index.js
@@ -10,6 +10,9 @@ import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
+import Appointment from "components/Appointment/index";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 // ----------------------------------------------------------------------------------
 
@@ -67,13 +70,13 @@ storiesOf("DayList", module)
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
   })
   .add("Monday", () => (
-    <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Monday"} onChange={action("setDay")} />
   ))
   .add("Tuesday", () => (
-    <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Tuesday"} onChange={action("setDay")} />
   ))
   .add("Wednesday", () => (
-      <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+    <DayList days={days} value={"Wednesday"} onChange={action("setDay")} />
   ));
 
 // ----------------------------------------------------------------------------------
@@ -133,14 +136,26 @@ storiesOf("InterviewerList", module)
   .add("Selected", () => (
     <InterviewerList
       interviewers={interviewers}
-      interviewer={3}
+      value={3}
     />
   ))
   .add("Clickable", () => (
     <InterviewerList
       interviewers={interviewers}
-      setInterviewer={action("setInterviewer")}
+      onChange={action("setInterviewer")}
     />
   ));
 
+// ----------------------------------------------------------------------------------
+
+storiesOf("Appointment", module)
+.addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+.add("Appointment", () => <Appointment />)
+.add("Appointment with Time", () => <Appointment time="12pm" />)
+.add("Header", () => <Header time="12pm" />)
+.add("Empty", ()=>(
+  <Empty onAdd={action("onAdd")} />
+))
 // ----------------------------------------------------------------------------------
